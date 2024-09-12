3fa0edb2a781247110ac681d90b675f1f6421a57
Marcio Bueno <marciobueno@gmail.com>
2024-09-12 09:02:30 -0300

    matriz transposta, busca em matriz

--- ex08/ex08-matriz01.c
+++ ex08/ex08-matriz01.c
@@ -8,12 +8,20 @@
 void lerMatriz(int mat[][QTD_COLUNAS], int lin, int col);
 void preencherMatrizAleatoria(int mat[][QTD_COLUNAS], int lin, int col);
 void imprimirMatriz(int mat[][QTD_COLUNAS], int lin, int col);
-
+void imprimirVetorInt(int v[], int tam);
+void imprimirVetorDouble(double v[], int tam);
+void calcularSomatorioMediaColuna(int mat[][QTD_COLUNAS], int lin, int col, int soma[], double media[]);
+void buscaMatriz(int mat[][QTD_COLUNAS], int lin, int col, int valor, int pos[2]);
+void transposta(int mat[][QTD_COLUNAS], int lin, int col, int matTrasp[][QTD_LINHAS]);
+void transpostaInPlace(int mat[][QTD_COLUNAS], int ordem);
 int lerDentroIntervalo(int min, int max);
 
 int main() {
     int qtdLinhas, qtdColunas;
     int matriz[QTD_LINHAS][QTD_COLUNAS];
+    int matrizTransposta[QTD_COLUNAS][QTD_LINHAS];
+    int somatorio[QTD_COLUNAS];
+    double media[QTD_COLUNAS];
 
     // deve ser utilizado quando você quiser que a cada execução gere uma sequência diferente
     // de números aleatórios
@@ -28,7 +36,20 @@ int main() {
     preencherMatrizAleatoria(matriz, qtdLinhas, qtdColunas);
     printf("\n--------------------\n");
     imprimirMatriz(matriz, qtdLinhas, qtdColunas);
-
+    printf("\n--------------------\n");
+    calcularSomatorioMediaColuna(matriz, qtdLinhas, qtdColunas, somatorio, media);
+    printf("Somatório por coluna: ");
+    imprimirVetorInt(somatorio, qtdColunas);
+    printf("Média por coluna: ");
+    imprimirVetorDouble(media, qtdColunas);
+    printf("\n--------------------\n");
+    transposta(matriz, qtdLinhas, qtdColunas, matrizTransposta);
+    imprimirMatriz(matrizTransposta, qtdColunas, qtdLinhas);
+    if (qtdLinhas == qtdColunas) {
+        printf("\n--------------------\n");
+        transpostaInPlace(matriz, qtdLinhas);
+        imprimirMatriz(matriz, qtdColunas, qtdLinhas);
+    }
     return 0;
 }
 
@@ -58,6 +79,21 @@ void imprimirMatriz(int mat[][QTD_COLUNAS], int lin, int col) {
     }
 }
 
+void imprimirVetorInt(int v[], int tam) {
+    for (int i = 0; i < tam; ++i) {
+        printf("%d\t", v[i]);
+    }
+    printf("\n");
+}
+
+void imprimirVetorDouble(double v[], int tam) {
+    for (int i = 0; i < tam; ++i) {
+        printf("%.2f\t", v[i]);
+    }
+    printf("\n");
+}
+
+
 int lerDentroIntervalo(int min, int max) {
     int valor;
     scanf("%d", &valor);
@@ -66,4 +102,47 @@ int lerDentroIntervalo(int min, int max) {
         scanf("%d", &valor);
     }
     return valor;
+}
+
+void calcularSomatorioMediaColuna(int mat[][QTD_COLUNAS], int lin, int col, int soma[], double media[]) {
+    for (int j = 0; j < col; ++j) {
+        soma[j] = 0;
+        for (int i = 0; i < lin; ++i) {
+            soma[j] += mat[i][j];
+        }
+        media[j] = (double) soma[j] / (double) lin;
+    }
+}
+
+void buscaMatriz(int mat[][QTD_COLUNAS], int lin, int col, int valor, int pos[2]) {
+    for (int i = 0; i < lin; ++i) {
+        for (int j = 0; j < col; ++j) {
+            if (mat[i][j] == valor) {
+                pos[0] = i;
+                pos[1] = j;
+                return;
+            }
+        }
+    }
+    pos[0] = -1;
+    pos[1] = -1;
+}
+
+void transposta(int mat[][QTD_COLUNAS], int lin, int col, int matTransp[][QTD_LINHAS]) {
+    for (int i = 0; i < lin; ++i) {
+        for (int j = 0; j < col; ++j) {
+            matTransp[j][i] = mat[i][j];
+        }
+    }
+}
+
+void transpostaInPlace(int mat[][QTD_COLUNAS], int ordem) {
+    int aux;
+    for (int i = 0; i < ordem - 1; ++i) {
+        for (int j = i + 1; j < ordem; ++j) {
+            aux = mat[i][j];
+            mat[i][j] = mat[j][i];
+            mat[j][i] = aux;
+        }
+    }
 }
